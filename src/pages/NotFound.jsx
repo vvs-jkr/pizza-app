0ff99961bc7de3775742441474b3aad5f1e001b1
VS <vvs.jkr@gmail.com>
2024-03-31 00:13:27 +0300

    add Skeleton, add Router

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NotFoundBlock from '../components/NotFoundBlock'
+
+const NotFound = () => {
+  return (
+    <div>
+      <NotFoundBlock />
+    </div>
+  )
+}
+
+export default NotFound
