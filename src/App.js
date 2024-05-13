4bbc8e1e950ed5c3ae5e9b3c929493fed52343f4
VS <vvs.jkr@gmail.com>
2024-05-13 17:11:39 +0300

    add useParams, useLocation, Outlet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import Header from './components/Header'
 import Home from './pages/Home'
-import NotFound from './pages/NotFound'
 import Cart from './pages/Cart'
+import FullPizza from './pages/FullPizza'
+import NotFound from './pages/NotFound'
 
 import { Routes, Route } from 'react-router-dom'
-import React, { useState } from 'react'
+import React from 'react'
 
 import './scss/app.scss'
+import MainLayout from './layouts/MainLayout'
 
 function App() {
   return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="" element={<Home />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="pizza/:id" element={<FullPizza />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   )
 }
 
