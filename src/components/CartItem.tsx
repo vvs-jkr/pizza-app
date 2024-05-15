88f0ee8d038820714680d23d686c296f6c37b57a
VS <vvs.jkr@gmail.com>
2024-05-15 17:53:17 +0300

    TypeScript + Redux Toolkit

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import { addItem, minusItem, removeItem } from '../redux/slices/cartSlice'
+import {
+  addItem,
+  CartItem,
+  minusItem,
+  removeItem,
+} from '../redux/slices/cartSlice'
 
 type CartItemProps = {
   id: string
@@ -12,7 +17,7 @@ type CartItemProps = {
   imageUrl: string
 }
 
-const CartItem: React.FC<CartItemProps> = ({
+const CartItemBlock: React.FC<CartItemProps> = ({
   id,
   title,
   type,
@@ -26,7 +31,7 @@ const CartItem: React.FC<CartItemProps> = ({
     dispatch(
       addItem({
         id,
-      })
+      } as CartItem)
     )
   }
 
@@ -126,4 +131,4 @@ const CartItem: React.FC<CartItemProps> = ({
   )
 }
 
-export default CartItem
+export default CartItemBlock
