a2f89e99d2f454c3b7e556598d7959df2b7bbd48
VS <vvs.jkr@gmail.com>
2024-05-17 18:37:21 +0300

    add LocalStorage + refactoring

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,27 @@
 import { Link, useLocation } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import { selectCart } from '../redux/slices/cartSlice'
 import logoSvg from '../assets/img/pizza-logo.svg'
 import Search from './Search'
+import React from 'react'
+import { selectCart } from '../redux/cart/selectors'
 
 function Header() {
   const { items, totalPrice } = useSelector(selectCart)
   const location = useLocation()
+  const isMounted = React.useRef(false)
 
   const totalCount = items.reduce(
     (sum: number, item: any) => sum + item.count,
     0
   )
+
+  React.useEffect(() => {
+    if (isMounted.current) {
+      const json = JSON.stringify(items)
+      localStorage.setItem('cart', json)
+    }
+    isMounted.current = true
+  }, [items])
 
   return (
     <div className="header">
@@ -26,7 +36,7 @@ function Header() {
           </div>
         </Link>
 
-        <Search />
+        {location.pathname !== '/cart' && <Search />}
         <div className="header__cart">
           {location.pathname !== '/cart' && (
             <Link to="/cart" className="button button--cart">
--- src/redux/cart/slice.ts
+++ src/redux/cart/slice.ts
@@ -1,25 +1,10 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { RootState } from '../store'
+import { getCartFromLS } from '../../utils/getCartFromLS'
+import { calcTotalPrice } from '../../utils/calcTotalPrice'
+import { CartItem, CartSliceState } from './types'
 
-export type CartItem = {
-  id: string
-  title: string
-  price: number
-  imageUrl: string
-  type: string
-  size: number
-  count: number
-}
-
-interface CartSliceState {
-  totalPrice: number
-  items: CartItem[]
-}
-
-const initialState: CartSliceState = {
-  totalPrice: 0,
-  items: [],
-}
+const initialState: CartSliceState = getCartFromLS()
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -37,9 +22,7 @@ const cartSlice = createSlice({
         })
       }
 
-      state.totalPrice = state.items.reduce((sum, obj) => {
-        return obj.price * obj.count + sum
-      }, 0)
+      state.totalPrice = calcTotalPrice(state.items)
     },
 
     minusItem(state, action: PayloadAction<string>) {
@@ -59,10 +42,6 @@ const cartSlice = createSlice({
     },
   },
 })
-
-export const selectCart = (state: RootState) => state.cart
-export const selectCartItemById = (id: string) => (state: RootState) =>
-  state.cart.items.find((obj) => obj.id === id)
 
 export const { addItem, removeItem, minusItem, clearItems } = cartSlice.actions
 
